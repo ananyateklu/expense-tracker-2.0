fbbe2694fb78205aae767006ac27af2112ee1cf8
Ananya Teklu <teklu009@morris.umn.edu>
2021-05-04 12:00:58 -0500

    Change amount type to double

--- Domain/Expense.cs
+++ Domain/Expense.cs
@@ -7,7 +7,7 @@ namespace Domain
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string ExpenseType { get; set; }
-        public int Amount { get; set; }
+        public double Amount { get; set; }
         public string Description { get; set; }
         public DateTime Date { get; set; }
         
