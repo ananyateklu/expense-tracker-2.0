af1b0744c3853f02fd684a0b75888e8596e4bc09
Ananya Teklu <teklu009@morris.umn.edu>
2021-05-08 23:59:55 -0500

    Create updated expense list with good ui

--- client-app/src/app/layouts/ExpenseList.tsx
+++ client-app/src/app/layouts/ExpenseList.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Expense } from '../models/expense';
 import axios from 'axios';
+import dateFormat from 'dateformat';
 
 
 
@@ -10,19 +11,37 @@ const ExpenseList = () => {
 
     useEffect(() => {
         axios.get<Expense[]>('http://localhost:5000/api/expenses').then(response => {
-          setExpenses(response.data);
+            setExpenses(response.data);
         })
-      }, [])
+    }, [])
+
 
     return <div className="ExpenseDiv">
-    {expenses.map(expense  => (
-      <div className="ExpenseList" key={expense.id}>
-        {expense.name}
-      </div>
-    ))}
-  </div>
-       
-    
+        {expenses.map(expense => (
+            <div className="ExpenseList" key={expense.id}>
+                <div className="entries">
+                    <div className="items">
+                        <div className="etype">
+                            <img alt="icons" src={`/assets/${expense.expenseType}.png`} />
+                        </div>
+                        <img className="remove" alt="remove" src="https://img.icons8.com/fluent-systems-regular/50/4a90e2/filled-trash.png" />
+                        <div className="typeamount">
+                            <small className="type">$ {expense.amount}</small>
+
+                            <h6 className="iname">{expense.name}</h6>
+
+                            <small>{(dateFormat(expense.date, "mmmm dS"))}</small>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        ))}
+    </div>
+
+
 
 }
 
