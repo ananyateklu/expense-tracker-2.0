215358d484dfbec2ac7c1624c5d86e7e62da6f0c
Ananya Teklu <teklu009@morris.umn.edu>
2021-05-10 14:08:37 -0500

    Create TotalExpense.tsx that get total expenses This will output the total expense on the client

---
+++ client-app/src/app/layouts/TotalExpense.tsx
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { TotalExpense } from '../models/totalexpense';
+
+
+const TExpense = () => {
+    const [totalExpenses, setTotalExpenses] = useState<TotalExpense[]>([]);
+
+    useEffect(() => {
+    
+        axios.get<TotalExpense[]>('http://localhost:5000/api/TotalExpense').then(response => {
+            setTotalExpenses(response.data);
+           
+        })
+    }, [])
+
+    return <div>
+        {totalExpenses.map(totalExpense => ( 
+        <div className='TotalExpenseDiv' key={totalExpense.id}><h1 style={{fontSize: '35px', display: 'inline'}}> <img style={{display: 'inline', width: '25px', padding: '2px', margin: '0', border: '1px solid green', borderRadius: '60%'}} alt="money" src="/assets/money.png"/> $ {totalExpense.utility}</h1><h5>Total Expenses</h5></div>
+        ))}
+    
+    </div>
+
+}
+
+export default TExpense;
