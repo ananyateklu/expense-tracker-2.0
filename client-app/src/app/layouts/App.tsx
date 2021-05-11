45b43e1b46ccd4f09de9a2b2ac881094d625b59f
Ananya Teklu <teklu009@morris.umn.edu>
2021-05-11 10:36:44 -0500

    Add Loading Component This will appear until the client fetches data from the server

--- client-app/src/app/layouts/App.tsx
+++ client-app/src/app/layouts/App.tsx
@@ -7,17 +7,22 @@ import TExpense from './TotalExpense';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { TotalExpense } from '../models/totalexpense';
+import Loading from './Loading';
 
 function App() {
   const [totalExpenses, setTotalExpenses] = useState<TotalExpense[]>([]);
+  const [loading, setLoading] = useState(true);
+
+
   useEffect(() => {
     axios.get<TotalExpense[]>('http://localhost:5000/api/TotalExpense').then(response => {
       setTotalExpenses(response.data);
 
+      setLoading(false);
     })
   }, [])
 
-
+  if (loading) return <Loading content='Loading app'/>
   const chartData = totalExpenses.map((type) => (<Donat key="Donat" food={type.food} utility={type.utility} school={type.school} transport={type.transport} hobby={type.hobby} />))
   return (
 
