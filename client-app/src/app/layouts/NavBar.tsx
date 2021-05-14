63ad8efeae387bff31c34d20f079dde093ed43c7
Ananya Teklu <teklu009@morris.umn.edu>
2021-05-14 17:51:42 -0500

    Add UI component for adding expenses

--- client-app/src/app/layouts/NavBar.tsx
+++ client-app/src/app/layouts/NavBar.tsx
@@ -1,31 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import  Logo  from './assets/Logo.png';
+import { Button, Form, Popup, Segment } from 'semantic-ui-react';
+import Logo from './assets/Logo.png';
 
 
 
 export default function NavBar() {
-    return (
-        <header className="head">
-        <Link className="logo" to="/">
-          <img src={Logo} alt="logo" />
+  return (
+    <header className="head">
+      <Link className="logo" to="/">
+        <img src={Logo} alt="logo" />
+      </Link>
+      <div className="sidebuttons">
+        <Link className="links" to="/">
+          <Button color='teal' icon='home' content='Dashboard' size='small' />
         </Link>
-        <div className="sidebuttons">
-          <Link className="links" to="/">
-            <img
-              alt="expenses"
-              className="expenses"
-              src="./assets/dashboard.png"
-            /><h5>Dashboard</h5>
-          </Link>
-          <Link className="linkAdd" to="/create">
-            <img 
-              alt="addexpense"
-              className="addexpense"
-              src="./assets/addexpense.png"/>
-              <h5>Add Expense</h5> 
-          </Link>
-        </div>
-      </header>
-    )
+        <Link className="linkAdd" to="">
+          <Popup
+            trigger={
+              <Button color='blue' icon='add' content='Add Expense' size='small' />
+            }
+            content={
+              <Segment clearing>
+                <Form autoComplete='off' >
+                  <Form.Input placeholder='Name' name='name' />
+                  <Form.Input placeholder='Expense Type' name='expensetype' />
+                  <Form.Input placeholder='Amount' name='amount' />
+                  <Form.Input type='date' placeholder='Date' name='date' />
+                  <Form.TextArea placeholder='Description' name='description' />
+                  <Button floated='right' positive type='submit' content='Submit' />
+                  <Button floated='right' type='button' content='Cancel' />
+                </Form>
+              </Segment>}
+            on='click'
+            position='right center'
+          />
+        </Link>
+
+      </div>
+
+    </header>
+  )
 }
