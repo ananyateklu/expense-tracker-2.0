648a83716e3e978d14252f13c83c334b61003ffd
Ananya Teklu <teklu009@morris.umn.edu>
2021-05-13 20:24:15 -0500

    Create Top component to show login status

---
+++ client-app/src/app/layouts/Top.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+
+export default function Top() {
+    return (
+        <header className="Top">
+        <img alt='A' src='./assets/a.png'/>
+      </header>
+    )
+}
