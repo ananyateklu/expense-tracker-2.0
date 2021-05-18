2d27b882de11fc629bd2dac90166e06516fbade5
Ananya Teklu <teklu009@morris.umn.edu>
2021-05-18 16:12:27 -0500

    Create Functions for handling creating an expense

--- client-app/src/app/layouts/NavBar.tsx
+++ client-app/src/app/layouts/NavBar.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Form, Popup, Segment } from 'semantic-ui-react';
+import { Expense } from '../models/expense';
 import Logo from './assets/Logo.png';
 
 
+interface Props {
+    selectedExpense: Expense | undefined;
+    createOrEdit: (expense: Expense) => void;
+    submitting: boolean;
+}
 
-export default function NavBar() {
+
+export default function NavBar({ selectedExpense, createOrEdit, submitting}: Props) {
+
+  const initialState = selectedExpense ?? {
+        id: '',
+        name: '',
+        expenseType: '',
+        amount: 0,
+        description: '',
+        date: '2020-04-08'
+  }
+
+  const [expense, setExpense] = useState(initialState);
+
+  function handleSubmit() {
+    console.log(expense);
+    createOrEdit(expense);
+
+  }
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    const { name, value } = event.target;
+    setExpense({...expense, [name]: value})
+  }
+
   return (
     <header className="head">
       <Link className="logo" to="/">
@@ -22,14 +52,13 @@ export default function NavBar() {
             }
             content={
               <Segment clearing>
-                <Form autoComplete='off' >
-                  <Form.Input placeholder='Name' name='name' />
-                  <Form.Input placeholder='Expense Type' name='expensetype' />
-                  <Form.Input placeholder='Amount' name='amount' />
-                  <Form.Input type='date' placeholder='Date' name='date' />
-                  <Form.TextArea placeholder='Description' name='description' />
-                  <Button floated='right' positive type='submit' content='Submit' />
-                  <Button floated='right' type='button' content='Cancel' />
+                <Form  autoComplete='off' >
+                  <Form.Input placeholder='Name' value={expense.name} name='name' onChange={handleInputChange}/>
+                  <Form.Input placeholder='Expense Type' value={expense.expenseType} name='expenseType' onChange={handleInputChange}/>
+                  <Form.Input placeholder='Amount' value={expense.amount} name='amount' onChange={handleInputChange} />
+                  <Form.Input type='date' placeholder='Date' value={expense.date} name='date' onChange={handleInputChange} />
+                  <Form.TextArea placeholder='Description' value={expense.description} name='description' onChange={handleInputChange} />
+                  <Button onClick={handleSubmit} loading={submitting} floated='right' positive type='submit' content='Submit' />
                 </Form>
               </Segment>}
             on='click'
