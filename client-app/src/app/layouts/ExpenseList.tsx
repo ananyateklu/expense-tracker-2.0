be48cc83e2af12c6a2985da7d627bb48705888a8
Ananya Teklu <teklu009@morris.umn.edu>
2021-05-19 12:39:11 -0500

    Create a Transition styling when the page loads

--- client-app/src/app/layouts/ExpenseList.tsx
+++ client-app/src/app/layouts/ExpenseList.tsx
@@ -1,7 +1,7 @@
 import { SyntheticEvent, useEffect, useState } from 'react';
 import { Expense } from '../models/expense';
 import dateFormat from 'dateformat';
-import { Button, Icon } from 'semantic-ui-react';
+import { Button, Icon, Transition } from 'semantic-ui-react';
 import agent from '../api/agent';
 
 
@@ -11,10 +11,13 @@ const ExpenseList = () => {
     const [expenses, setExpenses] = useState<Expense[]>([]);
     const [submitting, setSubmitting] = useState(false);
     const [target, setTarget] = useState('');
+    const [visible, setvisible] = useState(false);
+
 
     useEffect(() => {
         agent.Expenses.list().then(response => {
             setExpenses(response);
+            setvisible(true);
         })
     }, [])
 
@@ -33,44 +36,46 @@ const ExpenseList = () => {
     }
 
     return (
-        <div className="ExpenseDiv">
-            <h4>Transactions</h4>
-            {expenses.map(expense => (
-                <div className="ExpenseList" key={expense.id}>
-                    <div className="entries">
-                        <div className="items">
-                            <div className="etype">
-                                <img alt="icons" src={`/assets/${expense.expenseType}.png`} />
-                            </div>
-                            <div className="typeamount">
-                            <small className="type">$ {expense.amount}</small>
-                            <div className="Tbutton">
-                                <Button
-                                    name={expense.id}
-                                    loading={submitting && target === expense.id}
-                                    onClick={(e: any) => handleActivityDelete(e, expense.id)} 
-                                    floated='right'
-                                    size= 'mini'
-                                    icon >
-                                    <Icon 
-                                          name='trash' />
+        <Transition visible={visible} animation='fade up' duration={1000}>
+            <div className="ExpenseDiv">
+                <h4>Transactions</h4>
+                {expenses.map(expense => (
+                    <div className="ExpenseList" key={expense.id}>
+                        <div className="entries">
+                            <div className="items">
+                                <div className="etype">
+                                    <img alt="icons" src={`/assets/${expense.expenseType}.png`} />
+                                </div>
+                                <div className="typeamount">
+                                    <small className="type">$ {expense.amount}</small>
+                                    <div className="Tbutton">
+                                        <Button
+                                            name={expense.id}
+                                            loading={submitting && target === expense.id}
+                                            onClick={(e: any) => handleActivityDelete(e, expense.id)}
+                                            floated='right'
+                                            size='mini'
+                                            icon >
+                                            <Icon
+                                                name='trash' />
 
-                                </Button>
-                            </div>
-                              
+                                        </Button>
+                                    </div>
 
-                                <h6>{expense.name}</h6>
 
-                                <small>{(dateFormat(expense.date, "mmmm dS"))}</small>
+                                    <h6>{expense.name}</h6>
+
+                                    <small>{(dateFormat(expense.date, "mmmm dS"))}</small>
+
+                                </div>
 
                             </div>
 
                         </div>
-
                     </div>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div>
+        </Transition>
     )
 
 }
