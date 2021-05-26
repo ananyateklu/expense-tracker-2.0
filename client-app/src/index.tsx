51b070b2ad15c55b8594b18897815dce977c16b1
Ananya Teklu <teklu009@morris.umn.edu>
2021-05-26 12:42:59 -0500

    Remove Strict Mode to remove error

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -5,9 +5,8 @@ import App from './app/layouts/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    <App />,
   document.getElementById('root')
 );
 
